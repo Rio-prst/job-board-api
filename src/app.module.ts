ff22af7bde8a53712d53e4a4d566189967aaf08a
Rio Prasetio <rioprst17@gmail.com>
2026-07-10 15:08:04 +0700

    feat(auth): add dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { RequestIdMiddleware } from './common/middlewares/request-id.middleware';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ZodExceptionFilter } from './common/filters/zod-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 @Module({
   imports: [
@@ -13,7 +16,17 @@ import { RequestIdMiddleware } from './common/middlewares/request-id.middleware'
     PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: ZodExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
