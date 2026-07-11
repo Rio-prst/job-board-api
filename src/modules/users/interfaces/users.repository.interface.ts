1658222f9c0ed9aec887d46b656a6ebb8e66375e
Rio Prasetio <rioprst17@gmail.com>
2026-07-11 12:31:37 +0700

    feat(users): add users repository

--- src/modules/users/interfaces/users.repository.interface.ts
+++ src/modules/users/interfaces/users.repository.interface.ts
@@ -16,5 +16,6 @@ export const IUsersRepository = Symbol('IUsersRepository');
 
 export interface IUsersRepository {
   findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
   updateById(id: string, data: UpdateUserInput): Promise<User>;
 }
