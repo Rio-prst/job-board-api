89ebc42253171ae74a3082bf60615ee714a4da40
Rio Prasetio <rioprst17@gmail.com>
2026-07-13 07:32:49 +0700

    feat(chat): add chat gateway

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatRepository } from './chat.repository';
+import { ChatGateway } from './chat.gateway';
 import { IChatService } from './interfaces/chat.service.interface';
 import { IChatRepository } from './interfaces/chat.repository.interface';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, AuthModule],
   controllers: [ChatController],
   providers: [
+    ChatGateway,
     {
       provide: IChatService,
       useClass: ChatService,
